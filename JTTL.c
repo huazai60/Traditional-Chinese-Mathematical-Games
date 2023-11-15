88c30a84df66b4f8627ecb35c7b212ec91730cef
huazai60 <81062219+huazai60@users.noreply.github.com>
2023-11-15 15:10:20 +0800

    Add files via upload

---
+++ JTTL.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int Z=1;
+    printf("***********鸡兔同笼**********\n");
+    while(Z)
+    {
+        int x,y;
+        int feet,head;
+        printf("请输入头的数量：");
+        scanf("%d",&head);
+        printf("请输入脚的数量：");
+        scanf("%d",&feet);
+        x=(4*head-feet)/2;
+        y=head-x;
+        printf("鸡的数量为：%d\n兔的数量为：%d\n",x,y);
+        printf("结束程序请按0，再次计算请按1\n");
+        scanf("%d",&Z);
+        system("CLS");
+    }
+}
